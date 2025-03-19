b5b9e88ade1e648dbe8136e0c26275fb3c338ddb
Vidhi Jain <vidhijain507@gmail.com>
2025-03-19 21:54:33 +0530

    [Vidhi_Jain]Add. initial spring learning demo project.

---
+++ demo/src/main/java/com/capgeminitraining/Demo/controller/AlienController.java
@@ -0,0 +1,31 @@
+package com.capgeminitraining.Demo.controller;
+
+import com.capgeminitraining.Demo.AlienRepo;
+import com.capgeminitraining.Demo.model.Alien;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+//@Controller
+public class AlienController {
+    @Autowired
+    AlienRepo repo;
+    @GetMapping("aliens")
+    //@ResponseBody
+    public List<Alien> getAliens() {
+       List<Alien> aliens = repo.findAll();
+        return aliens;
+    }
+    @GetMapping("alien/{aid}")
+    //@ResponseBody
+    public Alien getAlien(@PathVariable("aid") int aid){
+        Alien alien = repo.findById(aid).orElse(new Alien("",0));
+        return alien;
+    }
+    @PostMapping("alien")
+    public Alien addAlien(@RequestBody   Alien alien){
+        repo.save(alien);
+        return alien;
+    }
+}
